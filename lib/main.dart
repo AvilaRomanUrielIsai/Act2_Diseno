059fe4c21b058c7d9307623010aa634a6a9eeb0c
URIEL_ISAI_AVILA_ROMAN <a.20308051280483@cbtis128.edu.mx>
2023-05-16 16:12:34 +0000

    v1

--- lib/main.dart
+++ lib/main.dart
@@ -1,92 +1,175 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+/// Flutter code sample for [BottomNavigationBar].
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  static const String _title = 'Flutter Stateful Clicker Counter';
-  // This widget is the root of your application.
+void main() => runApp(const Diseno());
+
+class Diseno extends StatelessWidget {
+  const Diseno({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: _title,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Inicio(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-  // This class is the configuration for the state.
+class Inicio extends StatefulWidget {
+  const Inicio({super.key});
+
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<Inicio> createState() => _InicioState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _InicioState extends State<Inicio> {
+  int _pagina = 0;
+  static final List<Widget> _contenido = <Widget>[
+    Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Container(
+          child: const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              "Uriel Isaí Ávila Román",
+              style: TextStyle(
+                fontSize: 30,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+          ),
+          decoration: BoxDecoration(
+            color: const Color(0x48fffba8),
+            borderRadius: BorderRadius.circular(15),
+            boxShadow: const [BoxShadow(color: Colors.black12, blurRadius: 5)],
+          ),
+        ),
+        const CircleAvatar(
+          radius: 100,
+          backgroundImage: AssetImage("assets/20230509_144403.jpg"),
+        ),
+        Container(
+          child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(children: const [
+                Text(
+                  "6-I",
+                  style: TextStyle(
+                    fontSize: 26,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+                Text(
+                  "Especialidad: Programación",
+                  style: TextStyle(
+                    fontSize: 26,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+              ])),
+          decoration: BoxDecoration(
+            color: const Color(0x48ece44a),
+            borderRadius: BorderRadius.circular(15),
+            boxShadow: const [BoxShadow(color: Colors.black12, blurRadius: 5)],
+          ),
+        ), //Especialidad y grupo
+      ],
+    ),
+    const Text(
+      'Página en mantenimiento',
+    ),
+    const Text(
+      'Página en mantenimiento',
+    ),
+    const Text(
+      'Página en mantenimiento',
+    ),
+  ];
 
-  void _incrementCounter() {
+  void _presionado(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _pagina = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: const Color(0xffc3e5ff),
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text('Flutter Demo Click Counter'),
+        title: const Text('Blockbuster',
+            style: TextStyle(color: Color(0xfff6d412))),
+        centerTitle: true,
+        backgroundColor: const Color(0xff0f6cf8),
+        leading: IconButton(
+          icon: const Icon(Icons.home, color: Color(0xfff9fd16)),
+          tooltip: 'Inicio',
+          onPressed: () {
+            Navigator.pushNamed(context, '/');
+          },
+        ),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: TextStyle(fontSize: 25),
+      endDrawer: Drawer(
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: const [
+            UserAccountsDrawerHeader(
+              // <-- SEE HERE
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/drawer.jpeg"), fit: BoxFit.cover),
+              ),
+              accountName: Text(
+                "Uriel Ávila",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              accountEmail: Text(
+                "Blockbuster_admin@gmail.com",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              currentAccountPicture: CircleAvatar(
+                  backgroundImage: AssetImage("assets/20230509_144403.jpg")),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+      body: Center(
+        child: _contenido.elementAt(_pagina),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Inicio',
+            backgroundColor: Color(0xff0f6cf8),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.movie),
+            label: 'Peliculas',
+            backgroundColor: Color(0xff0f6cf8),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.local_movies),
+            label: 'Series',
+            backgroundColor: Color(0xff0f6cf8),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Ajustes',
+            backgroundColor: Color(0xff0f6cf8),
+          ),
+        ],
+        currentIndex: _pagina,
+        selectedItemColor: const Color(0xffffee00),
+        onTap: _presionado,
       ),
     );
   }
 }
-
